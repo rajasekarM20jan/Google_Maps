bd93cf79698f002b5ce33bf4d48d4419bf5b0d9d
Rajasekar <rs20jn1998s@gmail.com>
2023-05-10 19:10:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/googlemaps/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.googlemaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    public static final String Latitude = "latitude";
+    public static final String Longitude = "longitude";
+    SharedPreferences sharedpreferences;
+    public static final int REQUEST_PERMISSION = 100;
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this, MapPage.class));
+        //method to request permission
+        requestPermissions();
+
+        getUserLocationForEvery2minutes();
+    }
+
+    // To update user current location for every minute.
+    private void getUserLocationForEvery2minutes() {
+        try {
+            // Handler to init the user location method for every 2 min.
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    // To fetch the user location.
+                    getLocation();
+                    // For every 2 min user location will be updated.
+                    handler.postDelayed(this, 12000);
+                }
+            }, 1000);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }// End Of getUserLocationForEvery2minutes().
+
+    // To get user location details.
+    public void getLocation() {
+        try {
+            // Getting the location manager to fetch the user location.
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            // To check if the user has given the location access or not.
+            // if not the method will not execute further.
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            // Line to get the location update using Network provider.
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000L, 5F, (LocationListener) this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }// End of getLocation().
+
+    //checking if the internet service is provided
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager cm;
+        try {
+            cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }// End of isNetworkConnected().
+
+    //checking the gps status
+    public static boolean checkGPSStatus(Context context) {
+        try {
+            LocationManager locationManager = null;
+            // Boolean value to store the GPS state and Network state.
+            boolean gps_enabled = false;
+            boolean network_enabled = false;
+            // Getting the location manager.
+            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            // Checking if the location and network is available or not and storing the boolean accordingly.
+            try {
+                gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            try {
+                network_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            // returning the values.
+            return gps_enabled || network_enabled;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }// End of checkGPSStatus().
+    //checking the permissions
+    public void requestPermissions() {
+        List<String> unGrantedPermissions = new ArrayList<>();
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                unGrantedPermissions.add(permission);
+            }
+        }
+
+        if (!unGrantedPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(this, unGrantedPermissions.toArray(new String[0]), REQUEST_PERMISSION);
+        }
+    }
+
+    //on result of permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions();
+                }
+            }
+        }
+    }
+
+    // On location change update the value.
+
+
+}
---
+++ app/src/main/java/com/example/googlemaps/MapPage.java
@@ -0,0 +1,239 @@
+package com.example.googlemaps;
+
+import static com.example.googlemaps.MainActivity.checkGPSStatus;
+import static com.example.googlemaps.MainActivity.isNetworkConnected;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MapPage extends AppCompatActivity {
+    LocationManager locationManager;
+    ImageView refreshButton;
+    String apiKey;
+    RequestQueue requestQueue;
+    AutocompleteSupportFragment autocompleteFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_page);
+        refreshButton=findViewById(R.id.refreshButton);
+        refreshButton.setOnClickListener(l->init());
+        apiKey="AIzaSyA6ALVPqgd1jyJ0ODOiHvdriutXitlFDLc";
+        requestQueue = Volley.newRequestQueue(this);
+        init();
+        if (!Places.isInitialized()) {
+
+            Places.initialize(getApplicationContext(), apiKey);
+        }
+    }
+
+    void init() {
+        try {
+            if (isNetworkConnected(this)) {
+                if (checkGPSStatus(this)) {
+                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    Criteria criteria = new Criteria();
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+                        LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                                .findFragmentById(R.id.map);
+                        mapFragment.getMapAsync(new OnMapReadyCallback() {
+                            @Override
+                            public void onMapReady(GoogleMap googleMap) {
+                                googleMap.clear();
+                                // Add a marker at current location fetched previously and animate the camera
+                                MarkerOptions markerOptions = new MarkerOptions();
+                                markerOptions.position(currentLatLng);
+                                markerOptions.title("You");
+                                googleMap.addMarker(markerOptions);
+                                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLatLng, 18);
+                                googleMap.animateCamera(cameraUpdate);
+
+                                autocompleteFragment =
+                                        (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+                                assert autocompleteFragment != null;
+                                autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+                                autocompleteFragment.setCountries("IN"); // Restrict to the INDIA
+                                autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS); // Only return ADDRESS
+                                autocompleteFragment.setTypeFilter(TypeFilter.CITIES); // Only return CITIES
+                                autocompleteFragment.setHint("Enter a location");
+                                autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                                    @Override
+                                    public void onPlaceSelected(@NonNull Place place) {
+                                        // Handle the selected place
+                                        LatLng destinationLatLng = place.getLatLng();
+                                        googleMap.clear();
+                                        // Add a marker to the map at the selected destination
+                                        googleMap.addMarker(markerOptions);
+                                        googleMap.addMarker(new MarkerOptions()
+                                                .position(destinationLatLng)
+                                                .title(place.getName()));
+                                        // Move the camera to the selected destination
+                                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                                        builder.include(currentLatLng);
+                                        builder.include(destinationLatLng);
+                                        LatLngBounds bounds = builder.build();
+                                        int padding=100;
+                                        CameraUpdate cameraUpdate2 = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                                        googleMap.animateCamera(cameraUpdate2);
+                                        String origin = currentLatLng.latitude + "," + currentLatLng.longitude;
+                                        String destination = destinationLatLng.latitude + "," + destinationLatLng.longitude;
+                                        String requestUrl = "https://maps.googleapis.com/maps/api/directions/json?origin="
+                                                + origin + "&destination=" + destination + "&key=" + apiKey;
+
+                                        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, requestUrl, null,
+                                                new Response.Listener<JSONObject>() {
+                                                    @Override
+                                                    public void onResponse(JSONObject response) {
+                                                        // Parse the response to extract the polylines information
+                                                        String encodedPolyline = "";
+                                                        try {
+                                                            JSONObject route = response.getJSONArray("routes").getJSONObject(0);
+                                                            JSONObject polyline = route.getJSONObject("overview_polyline");
+                                                            encodedPolyline = polyline.getString("points");
+                                                        } catch (JSONException e) {
+                                                            e.printStackTrace();
+                                                        }
+
+                                                        // Draw the polylines on the map using the Polyline class
+                                                        List<LatLng> decodedPolyline = decodePolyline(encodedPolyline);
+                                                        PolylineOptions polylineOptions = null;
+
+                                                        polylineOptions = new PolylineOptions()
+                                                                .addAll(decodedPolyline)
+                                                                .color(Color.BLUE)
+                                                                .width(10);
+
+                                                        googleMap.addPolyline(polylineOptions);
+                                                    }
+                                                },
+                                                new Response.ErrorListener() {
+                                                    @Override
+                                                    public void onErrorResponse(VolleyError error) {
+                                                        Log.e(null, "Directions request failed.");
+                                                    }
+                                                });
+                                        //used for adding the JsonObject Request to Volley
+                                        requestQueue.add(request);
+
+                                    }
+
+                                    @Override
+                                    public void onError(@NonNull Status status) {
+                                        // Handle error
+                                    }
+                                });
+
+
+                            }
+                        });
+                    }else{
+                        MainActivity m=new MainActivity();
+                        m.requestPermissions();
+                    }
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            AlertDialog.Builder a=new AlertDialog.Builder(this)
+                    .setCancelable(false)
+                    .setMessage("Oops! Something went wrong. Please Try Again after sometime")
+                    .setNegativeButton("OK", (dialog, which) -> {
+                        finishAffinity();
+                    });
+            a.show();
+        }
+    }
+
+    //Method to decode the encoded polyline from API response
+    private List<LatLng> decodePolyline(String encodedPolyline) {
+        List<LatLng> points = new ArrayList<>();
+        int index = 0, len = encodedPolyline.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+
+            do {
+                b = encodedPolyline.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+
+            int dLat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dLat;
+
+            shift = 0;
+            result = 0;
+
+            do {
+                b = encodedPolyline.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+
+            int dLng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dLng;
+
+            LatLng point = new LatLng((double) lat / 1E5, (double) lng / 1E5);
+            points.add(point);
+        }
+
+        return points;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+}
